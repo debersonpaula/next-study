bc46a52e7005910441cb85905c7d7d0d690eaed2
Deberson de Paula <deberson.paula@gmail.com>
2021-04-13 21:42:22 -0300

    added data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,45 @@ import Link from 'next/link';
 import Layout from '../component/layout';
 import { siteTitle } from '../component/layout/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
       <section className={utilStyles.headingMd}>
         <p>This user have interesting post</p>
         <Link href="/posts/first-post">
           <a>POSTS</a>
         </Link>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
